4b0657846e984cf778febe02753de726a556f7aa
Aleksieiev-Valentyn <104580477+drukkhua@users.noreply.github.com>
2022-06-28 16:55:26 +0300

    Add files via upload
    Aleksieiev Valentyn - 1st commit :)

---
+++ home_work_1_1.py
@@ -0,0 +1,15 @@
+'''Task #1 (Aleksieiev Valentin)
+Define variables a and b. Read values a and b from console and calculate: 
+a + b, a - b, a * b, a / b. 
+   Output obtained results.
+'''
+
+a, b = int(input(f'Input variable a: ')), int(input(f'Input variable b: '))
+for i in '+-*/':
+    print(f'a {i} b = {round(eval(str(a) + i + str(b)),2)}')
+    
+
+# print(f'a + b = {a + b}')
+# print(f'a - b = {a - b}')
+# print(f'a * b = {a * b}')
+# print(f'a / b = {a / b}')
