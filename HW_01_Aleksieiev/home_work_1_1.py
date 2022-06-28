88ccebaea77d1fedf84a30f1ac7437680d57dc56
Aleksieiev-Valentyn <104580477+drukkhua@users.noreply.github.com>
2022-06-28 21:04:03 +0300

    Update home_work_1_1.py

--- HW_01_Aleksieiev/home_work_1_1.py
+++ HW_01_Aleksieiev/home_work_1_1.py
@@ -8,7 +8,7 @@ a, b = int(input(f'Input variable a: ')), int(input(f'Input variable b: '))
 for i in '+-*/':
     print(f'a {i} b = {round(eval(str(a) + i + str(b)), 2)}')
     
-
+# eval is very bad function, but it works in this case very nice:)
 # print(f'a + b = {a + b}')
 # print(f'a - b = {a - b}')
 # print(f'a * b = {a * b}')
