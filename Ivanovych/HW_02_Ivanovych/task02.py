221a5e9a16c007c88efaa0fa2a5bda9c421e7c60
OI-22 <oleksandr.ivanovich22@gmail.com>
2022-07-03 00:36:35 +0300

    first commit

---
+++ Ivanovych/HW_02_Ivanovych/task02.py
@@ -0,0 +1,8 @@
+#task_02
+who = input('What is your name? ')
+age = input('How old are you? ')
+city = input ('Where do you live? ')
+
+print ('Hello, ', who, '!' )
+print ('Your age is', age)
+print ('You live in', city)
