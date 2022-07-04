03b1ed89bf089f1b945ec559ebefec4b2be0b6b8
Bohdan Ohorilko <bohdanohorilko8@gmail.com>
2022-07-04 16:50:22 +0300

    Added T1-T5 files

---
+++ Ohorilko/HW3/T1.py
@@ -0,0 +1,9 @@
+# User enters two numbers: a and b. Swap the values of variables a and b\
+#  without using the additional variable.
+
+
+listA = ['apple']
+listB = listA
+listA += ['pie'] 
+print(listA)
+print(listB)
