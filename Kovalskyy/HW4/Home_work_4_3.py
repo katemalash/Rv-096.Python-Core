60de15aaf8bb7b1846e88bf00f29bffaf55fe0e7
Stepan Kovalskyy <kovalskyyii@gmail.com>
2022-07-05 22:04:54 +0300

    changed wrong file

--- Kovalskyy/HW4/Home_work_4_3.py
+++ Kovalskyy/HW4/Home_work_4_3.py
@@ -6,13 +6,13 @@ if check_digit == 0:
 elif check_digit >= 1 and check_digit <10:
     print("positive one-digit")
 
-elif check_digit >= 1 and check_digit <100:
+elif check_digit >= 10 and check_digit <100:
     print("positive two-digit")
 
-elif check_digit >= -1 and check_digit <-10:
+elif check_digit <= -1 and check_digit >-10:
     print("negative one-digit")
 
-elif check_digit <= -1 and check_digit >-100:
+elif check_digit <= -10 and check_digit >-100:
     print("negative two-digit")
 
 else:
