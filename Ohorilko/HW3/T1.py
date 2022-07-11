6270bc15b4ecbf15074d4f4c7d39fc1659dc4246
Bohdan Ohorilko <bohdanohorilko8@gmail.com>
2022-07-11 16:26:25 +0300

    Added modifid files T1 and T4

--- Ohorilko/HW3/T1.py
+++ Ohorilko/HW3/T1.py
@@ -1,9 +1,9 @@
 # User enters two numbers: a and b. Swap the values of variables a and b\
 #  without using the additional variable.
 
+x = 5
+y = 10
 
-listA = ['apple']
-listB = listA
-listA += ['pie'] 
-print(listA)
-print(listB)
+x, y = y, x
+print("x =", x)
+print("y =", y)
--- Ohorilko/HW3/T4.py
+++ Ohorilko/HW3/T4.py
@@ -1,8 +1,8 @@
 # Calculate the area and perimeter of the circle over an entered radius. area = 3.14*r*r, perimeter 2*3.14*r
 
 radius = int(input("Please enter the radius of the circle (in centimeters): "))
-area = float(3.14 * pow(radius, 2))
-perimeter = float(2*3.14*radius)
+area = 3.14 * pow(radius, 2)
+perimeter = 2*3.14*radius
 print("The area is " , end='')
 print(area, end='')
 print (" centimeters squared")
