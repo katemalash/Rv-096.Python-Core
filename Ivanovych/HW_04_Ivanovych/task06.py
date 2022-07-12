cdeebe658135368477bc65aba9367b294da604bd
OI-22 <oleksandr.ivanovich22@gmail.com>
2022-07-12 16:59:31 +0300

    add

--- Ivanovych/HW_04_Ivanovych/task06.py
+++ Ivanovych/HW_04_Ivanovych/task06.py
@@ -1,14 +1,14 @@
 SeatNumber=int(input("Enter the number of your seat:"))
 
 if 1 <= SeatNumber <= 54:
-    if SeatNumber%2==0:
-        SeatPosition1='upper'
+    if SeatNumber%2 == 0:
+        SeatPosition1 ='upper'
     else:
-        SeatPosition1='lower'
+        SeatPosition1 ='lower'
     if SeatNumber<=36:
-        SeatPosition2='place in the compartment'
+        SeatPosition2 ='place in the compartment'
     else:
-        SeatPosition2='side seat'
+        SeatPosition2 ='side seat'
     print('You have a ',SeatPosition2,' on the ',SeatPosition1, 'berth ')
 else:
  print('You entered wrong number')
