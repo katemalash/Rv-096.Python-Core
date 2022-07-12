3a328e8fc11ca5e08bf260c12130a3ba2a1f14b8
OI-22 <oleksandr.ivanovich22@gmail.com>
2022-07-12 17:01:44 +0300

    taking away brackets

--- HW_03_Ivanovych/task04.py
+++ HW_03_Ivanovych/task04.py
@@ -4,8 +4,8 @@ import math
 
 radius = int(input('Enter radius of the circle'))
 
-area = math.pi * (radius ** 2)
-perimeter = (2 * (math.pi * radius))
+area = math.pi * radius ** 2
+perimeter = 2 * math.pi * radius
 
 print("The area of the circle is %.2f" %area, 'square centimeters.')
 print("Perimete of the cirkl is %.2f" %perimeter, 'square centimeters.')
