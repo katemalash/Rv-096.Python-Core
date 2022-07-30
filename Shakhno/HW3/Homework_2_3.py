570888aa211b9bc8ee38c2870d64f87f1e14236f
Michael Shachno <good.shahh@gmail.com>
2022-07-31 02:15:50 +0300

    area and perimeter triangle

---
+++ Shakhno/HW3/Homework_2_3.py
@@ -0,0 +1,8 @@
+height = float(input("Enter the height of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+area = height * width
+perimeter = (height + width)*2
+
+print("The area of a beautiful rectangle ", area, "M*2")
+print("The perimeter of a beautiful rectangle ", perimeter, "M")
