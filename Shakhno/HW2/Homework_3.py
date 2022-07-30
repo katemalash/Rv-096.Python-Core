3579a552b433a2bd4eac346864e2c16c33fedd6f
Michael Shachno <good.shahh@gmail.com>
2022-07-30 17:53:32 +0300

    HW2 (smile)

--- Shakhno/HW2/Homework_3.py
+++ Shakhno/HW2/Homework_3.py
@@ -1,8 +1,1 @@
-a = int(input())
-if a % 2 == 0:
-    if a % 4 == 0:
-        print("YES")
-    else:
-        print("NO")
-else:
-    print("NO")
+print('  _________\n /         \\\n |  /\\ /\\  |\n |    -    |\n |  \\___/  |\n \\_________/')
