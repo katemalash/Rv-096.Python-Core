502b99a059546050bd54b89e7ad22fa30e5b48e5
Stepan Kovalskyy <kovalskyyii@gmail.com>
2022-07-30 14:32:08 +0300

    Kovalskyy HW9 added

---
+++ Kovalskyy/HW9/Home_work_9_2.py
@@ -0,0 +1,29 @@
+# Suppose you are a cook and you have a lot of lettuce so you’ve decided to add it to all the dishes. 
+# Create a few collections that contain the ingredients of dish; 
+# create function that outputs the ingredients of each dish. 
+# Create decorator that adds lettuce to all the dishes.
+
+menu = {"Big_Mac":{"Bun", "Patty", "Sauce", "Cheese", "Pickle", "Onions"},
+        "Cheeseburger":{"Bun", "Patty", "Ketchup", "Cheese", "Pickle", "Onions", "Mustard"},
+        "Chicken_Sandwich":{"Bun", "Chicken", "Mayonnaise", "Tomato", "Lettuce"},
+        }
+
+def add_lettuce(func):
+    def wrapper(*args,**kwargs):
+        print('Collection before decorate:')
+        func(*args,**kwargs)
+        print (args)
+        menu = args[0]
+        for val in menu.values():
+            val.add("Lettuce")
+
+        print('Collection after decorate:')
+        return func(*args,**kwargs) 
+    return wrapper
+
+@add_lettuce
+def show_ing(ingr):
+    print("The ingrediants of each dish are:")
+    [print(f"{key} : {val}") for key, val in ingr.items()]
+
+show_ing(menu)
